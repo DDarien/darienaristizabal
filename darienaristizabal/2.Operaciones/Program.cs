cda08b82d4f46d6cf4912c4a1bc7f791ac4dcaee
DDarien <darien.aristizabal@upb.edu.co>
2025-08-10 18:52:07 -0500

    Condicionales

--- darienaristizabal/2.Operaciones/Program.cs
+++ darienaristizabal/2.Operaciones/Program.cs
@@ -7,67 +7,79 @@ namespace _2_Operadores
         static void Main(string[] args)
         {
             //OPERADORES
-
             //Operadores Numéricos
             //Cambio de Signo
+
             int num1 = 5;
             int num2 = -num1;
-            int num3 = +num1;
-            int num4 = -num2;
+            int num3 = +num2;
+            int num4 = -num3;
 
             Console.WriteLine($"{num1} // {num2} // {num3} // {num4}");
 
             //Operadores aritméticos
+
             int num5 = 3 + 5; //suma
             int num6 = 100 - 34; // resta
-            int num7 = 3 * 5; //Multiplicación
-            int num8 = (int)(float)5 / 3; //División
-            float num9 = 5f / 3;
-            double num10 = 5d / 3;
-            decimal num10b = 5m / 3m;
+            int num7 = 4 * 2; //Multiplicación
+            // float num8 =(float) 5 / 3; //División
+            // float num8 = 5f / 3
+            float num8 = 5f / 3f;
+            double num9 = 5d / 3;
+            decimal num10b = 5 / 3m;
 
-            Console.WriteLine($"suma: {num5}, resta: {num6}, producto: {num7}, division: {num8} // {num9} // {num10b}");
+            Console.WriteLine($"suma: {num5}, resta: // {num6}, producto: // {num7} //, division: {num8} // {num9} // {num10b}");
 
             //Operadores incremento(++), decremento(--)
             num1++; //num1 = num1 + 1; Incremento
             num2--; //num2 = num2 - 1; Decremento
-            num3 += 2; //num3 = num3 + 2;
-            num4 -= 3; //num4 = num4 - 3;
-            num5 *= 3; //num5 = num5 * 3;
-            num6 /= 7; //num6 = num6 / 7;
-            num7 %= num1; //num7 = num7 % num1;
+            num3 += 5; //num3 = num3 + 5;
+            num4 -= 20; //num4 = num4 -20;
+            num5 *= 31; //num5 = num5 * 31;
+            num7 /= num1; //num7 = num7 / num1;
 
             //Orden de Evaluación operadores
-            int num11 = 3 + 3 * 2;
-            int num12 = (3 + 3) * 2;
-            int num13 = 3 + 3 / 2;
-            int num14 = (3 + 3) / 2;
-            int num15 = 3 + (3 / 2);
-            int num16 = (3 + 3) / (2 + 1);
-
-            Console.WriteLine($"{num11}, {num12}, {num13}, {num14}, {num15}, {num16}");
+            int num11 = 4 * 3 / 2;// *,/
+            int num12 = 4 / 3 * 2;// /.*
+            int num13 = (4 * 3)/ 2;// ()./
+            int num14 = 4 * (3 / 2);// * (.) /
+            int num15 = 4 + 3 * 2;// + *
+            int num16 = 4 + 6 / 2;// + /
+            Console.WriteLine($"num11: {num11}, num12: {num12}, num13: {num13}, num14: {num14}, num15: {num15}, num16: {num16}");
 
             //Conjunción
             Console.WriteLine("TABLA DE VERDAD CONJUNCIÓN");
-            Console.WriteLine($"{false} && {false} = {false && false}");
-            Console.WriteLine($"{true} && {false} = {true && false}");
-            Console.WriteLine($"{false} && {true} = {false && true}");
-            Console.WriteLine($"{true} && {true} = {true && true}");
+            Console.WriteLine($"F && V = {false && true}");
+            Console.WriteLine($"F && F = {false && false}");
+            Console.WriteLine($"F && V = {false && true}");
 
             //Disyunción
             Console.WriteLine("TABLA DE VERDAD DISYUNCIÓN");
-            Console.WriteLine($"{false} || {false} = {false || false}");
-            Console.WriteLine($"{true} || {false} = {true || false}");
-            Console.WriteLine($"{false} || {true} = {false || true}");
-            Console.WriteLine($"{true} || {true} = {true || true}");
+            Console.WriteLine($"V || V = {true && true}");
+            Console.WriteLine($"V || F = {true && false}");
+            Console.WriteLine($"F || V = {false && true}");
+            Console.WriteLine($"F || F = {false && false}");
+            bool exp1 = true;
+            bool exp2 = false;
+            bool exp3 = exp1 && exp2;
+            bool exp4 = exp2 || exp3;
+            bool exp5 = !exp4 || exp2;
 
-            bool exp1 = true && true;
-            bool exp2 = false || false;
-            bool exp3 = true && false;
-            bool exp4 = false || true;
-            bool exp5 = exp1 || exp2;
+            Console.WriteLine($"exp1={exp1}, exp2={exp2}, exp3={exp3}, exp4={exp4}, exp5={exp5}");
 
-            Console.WriteLine($"exp1: {exp1}, exp2: {exp2}, exp3: {exp3}, exp4: {exp4}, exp5: {exp5}");
+            //OPERADORES DE COMPARACIÓN
+            bool exp6 = 5 > 4;
+            Console.WriteLine(exp6);
+            bool exp7 = 5>=5;
+            Console.WriteLine(exp7);
+            bool exp8 = 100 == 102;
+            Console.WriteLine(exp8);
+            bool exp9 = 100 !=102;
+            Console.WriteLine(exp9);
+            bool exp10 = 5 > 4 && 2 < 3;
+            Console.WriteLine(exp10);
+            bool exp11 = 5 > 4 || !exp4;
+            Console.WriteLine(exp11);
         }
     }
 }
---
+++ darienaristizabal/3.Condicionales/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONDICIONAL SIMPLE
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("ingrese el nombre");
+            nombre = Console.ReadLine();//TODO LO QUE YO LEO DESDE LA CONSOLA LLEGA COMO STRING
+            Console.WriteLine(nombre);
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+           if(sueldo>3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+        }
+    }
+}
