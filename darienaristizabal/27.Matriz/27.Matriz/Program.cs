d0b50d9906bb2c22f310a8e64979d8421b4fdca3
DDarien <darien.aristizabal@upb.edu.co>
2025-10-17 15:33:28 -0500

    Add files via upload

---
+++ darienaristizabal/27.Matriz/27.Matriz/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _27.Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número de filas de la matriz:");
+            int filas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número de columnas de la matriz:");
+            int columnas = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[filas, columnas];
+            int positivos = 0, negativos = 0, ceros = 0;
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.WriteLine($"Ingrese el número para la posición [{i},{j}]:");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                    if (matriz[i, j] > 0)
+                    {
+                        positivos++;
+                    }
+                    else if (matriz[i, j] < 0)
+                    {
+                        negativos++;
+                    }
+                    else
+                    {
+                        ceros++;
+                    }
+                }
+            }
+            Console.WriteLine($"Total de números positivos: {positivos}");
+            Console.WriteLine($"Total de números negativos: {negativos}");
+            Console.WriteLine($"Total de ceros: {ceros}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ darienaristizabal/28.ProgramacionModular/28.ProgramacionModular/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _28.ProgramacionModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje("Soy Darien");
+            MostrarMensaje("Tengo 22 años");
+            MostrarMensaje("Adiós Mundo");
+        }
+
+        static void MostrarMensaje(string mensaje)
+        {
+            Console.WriteLine("Bienvenido a la programación modular");
+            MostrarMensaje("Estoy siendo llamado desde MostrarMensaje");
+        }
+
+        static int CalcularEdad() 
+        {
+            int Edad = 2025 - 1981;
+            return Edad;
+        }
+
+        static int calcularEdad2(int añoActual, int añoNacimiento) 
+
+        {
+            return añoActual - añoNacimiento;
+        }
+    }
+}
